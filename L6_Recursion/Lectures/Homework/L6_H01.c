cf71a4a1564a2b8f11c9a036010232178c7167f0
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2025-11-30 12:27:46 +0530

    Printing n to 1 and then 1 to n using Recursion

---
+++ L6_Recursion/Lectures/Homework/L6_H01.c
@@ -0,0 +1,26 @@
+//First print n to 1 and then print 1 to n
+
+#include <stdio.h>
+
+void homework_rec(int n);
+
+int main(){
+
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    homework_rec(n);
+
+    return 0;
+}
+
+void homework_rec(int n){
+
+    if(n == 0) return;// base case
+
+    printf("%d\n", n);
+    homework_rec(n - 1);
+    printf("%d\n", n);
+    return;
+}
