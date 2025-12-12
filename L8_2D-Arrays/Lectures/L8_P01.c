ca19d9ea04651f9c640cc2edb2817964ff42de42
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2025-12-12 12:42:44 +0530

    Adding 2 matrices(Using extra matrix)

---
+++ L8_2D-Arrays/Lectures/L8_P01.c
@@ -0,0 +1,27 @@
+//Adding 2 matrices(Using extra matrix)
+
+#include <stdio.h>
+
+int main(){
+
+    int M1[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+    int M2[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+    int M3[3][3];
+
+    int i,j;
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j<3; j++){
+            M3[i][j] = M1[i][j] + M2[i][j];
+        }
+    }
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("%d ", M3[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
